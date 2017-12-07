78e0fcb5e9da0774494de753ca9a815749357622
Valery Krasnoselsky <valery.krasnoselsky@gmail.com>
2017-12-07 21:40:02 +0300

    updated engine configuration

--- src/config.h
+++ src/config.h
@@ -6,16 +6,16 @@
 // infinity value
 #define INF 999999
 // checkmate value
-#define CHECKMATE 50000
+#define CHECKMATE 5000
 
 #define CHECK_DEEPENING 1
 #define FORCED_MATE_DEEP 15
-#define NULL_MOVE_DEPTH 3
-#define FUTILITY_DEPTH 3
+#define NULL_MOVE_DEPTH 4
+#define FUTILITY_DEPTH 4
 
 #define PAWN_WEIGH 100
 #define KNIGHT_WEIGH 300
-#define BISHOP_WEIGH 300
+#define BISHOP_WEIGH 310
 #define ROOK_WEIGH 500
 #define QUEEN_WEIGH 900
 
